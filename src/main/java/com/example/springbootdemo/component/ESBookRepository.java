24fd62d05182a73953a91e8602294654d436f892
loveleekay <lysheng526@163.com>
2021-10-14 11:09:13 +0800

    整合ELK和ES

---
+++ src/main/java/com/example/springbootdemo/component/ESBookRepository.java
@@ -0,0 +1,24 @@
+package com.example.springbootdemo.component;
+
+import com.example.springbootdemo.dto.Book;
+import org.springframework.data.elasticsearch.annotations.Highlight;
+import org.springframework.data.elasticsearch.annotations.HighlightField;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ESBookRepository extends ElasticsearchRepository<Book, String> {
+
+    List<Book> findByTitleOrAuthor(String title, String author);
+
+    @Highlight(fields = {
+            @HighlightField(name = "title"),
+            @HighlightField(name = "author")
+    })
+    @Query("{\"match\":{\"title\":\"?0\"}}")
+    SearchHits<Book> find(String keyword);
+}
